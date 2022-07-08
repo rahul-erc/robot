bd3120cf2f315aef02825a09408491a6ecd5b5bc
rahul-erc <89387849+rahul-erc@users.noreply.github.com>
2022-07-09 01:10:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'tachyons' ;
 import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons'; 
 
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(    <App /> );
-
+ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();

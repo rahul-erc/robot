b5aa3f95e36da7f52576092185711a0e5aca7834
rahul-erc <eagalapatirahul@gmail.com>
2022-07-09 00:50:35 +0530

    first commit

---
+++ src/robots.js
@@ -0,0 +1,73 @@
+export const robots =[
+
+    {
+        id:1,
+        name:'Leannana Graham',
+        username:'bret',
+        email:'Sincere@april.com'
+    },
+
+    {
+        id:2,
+        name:'raminescene',
+        username:'remo',
+        email:'Sincare@gmail.com'
+    },
+
+    {
+        id:3,
+        name:'schrodginer',
+        username:'scrok',
+        email:'iamschrok@gmail.com'
+    },
+
+    {
+        id:4,
+        name:'Ervin howell',
+        username:'ervin',
+        email:'Shanna@melissa.tv'
+    },
+
+    {
+        id:5,
+        name:'Clementine blauch',
+        username:'Clement',
+        email:'Nathan@yesenia.com'
+    },
+    {
+        id:6,
+        name:'Kurtis Weissnat',
+        username:'kurtis',
+        email:'Telly.hoger@billy.com'
+    },
+
+    {
+        id:7,
+        name:'Nicholas runalf',
+        username:'Nic',
+        email:'Sherwood@ransom.com'
+    },
+    
+    {
+        id:8,
+        name:'Chelsey',
+        username:'Chel',
+        email:'Lucio_Hettin@annioe.co'
+    },
+
+    {
+        id:9,
+        name:'Patricia Lebsack',
+        username:'Patrick',
+        email:'Juliana.OConner@kory.org'
+    },
+
+    {
+        id:10,
+        name:'Manchetser',
+        username:'mcu',
+        email:'city@united.com'
+    },
+
+
+]

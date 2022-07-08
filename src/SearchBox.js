c0aefbef6da5c8f286a3c74cbb6030ed6a36144f
rahul-erc <89387849+rahul-erc@users.noreply.github.com>
2022-07-09 01:08:56 +0530

    Update SearchBox.js

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,20 +1,16 @@
-import React from "react";
+import React from 'react';
 
-
-const SearchBox = ({searchfield,searchchange}) => {
-    return(
-        <div className="pa2 ">
-            <input 
-                className="pa3 ba b--green bg-lightest-blue"
-                type='search' 
-                placeholder='search robots'
-                onChange={searchchange}
-             />
-        </div>
-        
-    );
+const SearchBox = ({ searchfield, searchChange }) => {
+  return (
+    <div className='pa2'>
+      <input
+        className='pa3 ba b--green bg-lightest-blue'
+        type='search'
+        placeholder='search robots'
+        onChange={searchChange}
+      />
+    </div>
+  );
 }
-
-
 
 export default SearchBox;
